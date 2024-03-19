adec13c70fc978004ba8e174ac24746e3273359b
Md Masrur Hasan <masrurhasan2002@gmail.com>
2024-03-19 22:49:59 +0200

    Add comment route

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const postsRouter = require('./routes/posts')
+const commentsRouter = require('./routes/comments')
 
 const app = express()
 
 const PORT = 3001
 
 app.use('/api/posts', postsRouter)
-
+app.use('/api/posts/:postId/comments', commentsRouter) 
 
 app.listen(PORT)
 console.log(`listening to port ${PORT}`)
